7863632fd5122259d51defa9f188abab97f721d3
Mario Andres <mariojaramillo15@gmail.com>
2025-12-10 13:50:28 -0500

    se implementa el modulo de dashboard

--- src/app/modules/dashboard/dashboard.ts
+++ src/app/modules/dashboard/dashboard.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  styleUrl: './dashboard.css',
 })
 export class Dashboard {
+   private router = inject(Router)
+user = JSON.parse(sessionStorage.getItem('user') || '{}');
+
+ logout() {
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
+
 
 }
